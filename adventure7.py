eacfdc0e5ef87954b944c0fa1d84da057d9e5945
Damon Salvatore <adventureinsiliconvalley@gmail.com>
2019-10-24 16:54:42 +0800

    commit

--- adventure7.py
+++ adventure7.py
@@ -1,3 +1,4 @@
+# encoding:utf-8
 import numpy as np
 
 # 说明：新加一个苹果的时候（比如从三个增加到四个），你可以用，也可以不用。如果你使用这个苹果，那么就等于四个苹果全部用（因为如果只是使用三个或者更少苹果的话，那么等于没有用到第四个苹果），那么总数已经有四个苹果了，其他的份数可以由梨和桃子补充。如果你没有使用这个苹果，那么总数还是由三个苹果、梨和桃子补充。四个苹果和其他固定数量的梨和桃子由这两个数之和获得。
@@ -12,39 +13,28 @@ import numpy as np
 
 m = np.zeros((9, 6, 5, 4))
 
+# For a total combination number of 0, all combinations of the three dishes can have one way to do it.
+# It's the first boundary condition.
 m[0][:] = 1
 
-for i in range(1, 9):
-    for j in range(1, 6):
-        if j >= i:
-            m[i][j][0][0] = 1
-
+# First dish and third dish with value of 0.
+# For all the numbers of the second dish, find the number of total combinations.
 for i in range(1, 9):
     for j in range(1, 5):
         if j >= i:
             m[i][0][j][0] = 1
 
-for i in range(1, 9):
-    for j in range(1, 4):
-        if j >= i:
-            m[i][0][0][j] = 1
-
+# Third dish of number 0.
+# For every number of the second dish, find the total number of combinations for the first dish with number from 1 to 5.
+# Now we have the number of total combinations for every combination of the first and the second dish
+#  with the third dish with number 0.
 for i in range(1, 9):
     for j in range(1, min(i + 1, 6)):
         for k in range(1, min(i + 1, 5)):
             m[i][j][k][0] += m[i][j - 1][k][0] + m[i - j][0][k][0]
 
-for i in range(1, 9):
-    for k in range(1, min(i + 1, 5)):
-        for n in range(1, min(i + 1, 4)):
-            print(i, k, n, m[i][0][k - 1][n], m[i - k][0][0][n])
-            m[i][0][k][n] += m[i][0][k - 1][n] + m[i - k][0][0][n]
-
-for i in range(1, 9):
-    for j in range(1, min(i + 1, 6)):
-        for n in range(1, min(i + 1, 4)):
-            m[i][j][0][n] += m[i][j - 1][0][n] + m[i - j][0][0][n]
-
+# For every combination of the first and second dish,
+# find the total number of combinations for the third dish with number from 1 to 3.
 for i in range(1, 9):
     for j in range(1, min(i + 1, 6)):
         for k in range(1, min(i + 1, 5)):
